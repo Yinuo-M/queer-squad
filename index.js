26f8d9f83d722a155f861e9ec4dc2f3bbc0eac66
Yinuo Meng <mengyn1999@gmail.com>
2023-04-21 15:42:21 +0100

    Style mobile nav

--- index.js
+++ index.js
@@ -5,14 +5,35 @@ const closeMenuBtn = document.querySelector('.nav__menu-button--close');
 
 if (mobileMenu && openMenuBtn) {
   const openMenu = () => {
-    mobileMenu.classList.add('nav__mobile-nav--open');
+    mobileMenu.classList.remove('hidden');
+
+    // Freeze body scroll
+    document.body.classList.add('scroll-freeze');
   };
   openMenuBtn.addEventListener('click', openMenu);
 }
 
 if (mobileMenu && closeMenuBtn) {
   const closeMenu = () => {
-    mobileMenu.classList.remove('nav__mobile-nav--open');
+    mobileMenu.classList.add('hidden');
+
+    // Defreeze body scroll
+    document.body.classList.remove('scroll-freeze');
   };
   closeMenuBtn.addEventListener('click', closeMenu);
+
+  const closeMenuOnClickOutside = (e) => {
+    if (e.target === mobileMenu) closeMenu();
+  };
+  mobileMenu.addEventListener('click', closeMenuOnClickOutside);
+}
+
+// Add event listeners to nav language switcher
+const languageBtn = document.querySelector('.nav__language-button');
+const languageList = document.querySelector('.nav__language-list');
+if (languageBtn && languageList) {
+  const toggleList = () => {
+    languageList.classList.toggle('hidden');
+  };
+  languageBtn.addEventListener('click', toggleList);
 }
